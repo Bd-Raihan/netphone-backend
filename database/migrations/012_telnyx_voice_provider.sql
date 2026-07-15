01bacdfad797e112ce7ed0a80bad682c8efb92cd
Bd-Raihan <YOUR_GITHUB_EMAIL>
2026-07-15 20:33:42 +0300

    Add Telnyx voice provider migration

---
+++ database/migrations/012_telnyx_voice_provider.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+ALTER TABLE call_sessions
+ADD COLUMN IF NOT EXISTS provider_call_id TEXT;
+
+CREATE INDEX IF NOT EXISTS idx_call_sessions_provider_call_id
+ON call_sessions(provider_call_id);
+
+-- পুরোনো Twilio SID থাকলে generic column-এ backup copy
+UPDATE call_sessions
+SET provider_call_id = twilio_call_sid
+WHERE provider_call_id IS NULL
+  AND twilio_call_sid IS NOT NULL;
+
+COMMIT;
